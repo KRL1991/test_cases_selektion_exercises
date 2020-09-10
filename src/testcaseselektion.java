2feecbb7e313c3b94ce88bd50acb7c550bdb9a5f
KRL1991 <kimx4706@edu.easj.dk>
2020-09-10 14:47:39 +0200

    Initial commit

---
+++ src/testcaseselektion.java
@@ -0,0 +1,66 @@
+ class testcaseselektion {
+     public static void main(String[] args) {
+
+     }
+
+
+
+    }
+
+    class Selektion1a {
+        public static void main(String[] args) {
+            System.out.println("Selektion_exercise1a");
+            int x = 5;
+            int y = 8;
+            int z = 3;
+
+            System.out.print(" a : ");
+            System.out.println(x <= 5 && z != y);
+            System.out.print(" b : ");
+            System.out.println(x == 5 || x == y && z == 3);
+            System.out.print(" c : ");
+            System.out.println(x / y > z / x);
+            System.out.print(" d : ");
+            System.out.println(!( x != y - z ) == false);
+            System.out.print(" e : ");
+            System.out.println(2 * x != x || x == 0);
+            System.out.print(" f : ");
+            System.out.println(! true || ! false);
+
+        }}
+        class Selektion1b {
+            public static void main(String[] args) {
+                System.out.println("Selektion exercise 1b");
+                int x = 7;
+                int y = 1;
+                int z = 0;
+
+                System.out.print("a : ");
+                System.out.println(x <= 5 && z != y);
+                System.out.print("b : ");
+                System.out.println(x == 5 || x == y && z == 3);
+                System.out.print("c : ");
+                System.out.println(x / y > z / x);
+                System.out.print("d : ");
+                System.out.println(!( x != y - z ) == false);
+                System.out.print("e : ");
+                System.out.println(2 * x != x || x == 0);
+                System.out.print("f : ");
+                System.out.println(! true || ! false);
+
+            }}
+
+
+            class Selektion2a{
+                public static void main(String[] args) {
+                    System.out.println("Selektion exercise 2a");
+                    int x = 41;
+                    int y = 67;
+
+                    System.out.println(x + y < 100);
+                    
+
+
+
+                }}
+
